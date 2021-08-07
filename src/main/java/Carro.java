98b597b4ba544f69d991b54133aad9cc9398fd97
Augusto Vasconcelos <augustovnd@gmail.com>
2021-08-06 22:33:28 -0300

    *

--- src/main/java/Carro.java
+++ src/main/java/Carro.java
@@ -1,39 +1,69 @@
 public class Carro {
 
+    private int passageiros = 0;
+
+    private int tanque = 0;
+
+    private int quilometragem = 0;
+
     public int getPassageiros() {
-        return -1;
+        return passageiros;
     }
 
     public int getCombustivel() {
-        return -1;
+        return tanque;
     }
 
     public int getQuilometragem() {
-        return -1;
+        return quilometragem;
     }
 
     public boolean embarcar() {
-        return false;
+        if(passageiros==2)
+            return false;
+        else {
+            passageiros++;
+            return true;
+        }
     }
 
     public boolean desembarcar() {
-        return false;
+        if(passageiros==0)
+            return false;
+        else {
+            passageiros--;
+            return true;
+        }
     }
 
     public boolean dirigir(int distancia) {
-        return false;
+        if(tanque<distancia) {
+            quilometragem+=tanque;
+            tanque=0;
+            return false;
+        }
+        else {
+            tanque-=distancia;
+            quilometragem+=distancia;
+            return true;
+        }
     }
 
     public boolean abastecer(int quantidade) {
-        return false;
+        if(quantidade<0)
+            return false;
+        else {
+            tanque=Math.min(tanque+quantidade,100);
+            return true;
+        }
     }
 
     @Override
     public String toString() {
         return "Carro{" +
-                "passageiros=" + -1 +
-                ", combustivel=" + -1 +
-                ", quilometragem=" + -1 +
+                "passageiros=" + passageiros +
+                ", combustivel=" + tanque +
+                ", quilometragem=" + quilometragem +
                 '}';
     }
 }
